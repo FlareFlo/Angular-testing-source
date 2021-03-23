5bc97b4d1c24c79f7d5fa6f843e691cf3cedb77e
FlareFlo <florianlipp17@gmail.com>
2021-03-23 20:58:24 +0100

    Added rickroll duck

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,22 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-footer',
-  template: `
-    <div class="container content has-text-centered is-size-7 has-text-centered">
-      Made by FlareFlo using Angular
-      <p>Click this duck :D</p>
-      <img src="/assets/duck_header.jpg" style="width: 10%;" alt="image">
-    </div>
-  `,
-  styles: [
-  ]
+    selector: 'app-footer',
+    template: `
+        <div class="container content has-text-centered is-size-7 has-text-centered">
+            Made by FlareFlo using Angular
+            <p>Click this duck :D</p>
+            <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">
+                <img src="/assets/duck_header.jpg" style="width: 10%;" alt="image">
+            </a>
+        </div>
+    `,
+    styles: []
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+    constructor() {
+    }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
 }
