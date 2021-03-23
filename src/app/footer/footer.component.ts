2950f033d4c6b096c33a140fd805eae7013760b8
FlareFlo <florianlipp17@gmail.com>
2021-03-23 20:29:59 +0100

    Build

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
     <div class="container content has-text-centered is-size-7 has-text-centered">
       Made by FlareFlo using Angular
       <p>Click this duck :D</p>
-      <img src="/assets/duck_header.jpg" style="width: 10%; min-width: 100px" alt="image">
+      <img src="/assets/duck_header.jpg" style="width: 10%;" alt="image">
     </div>
   `,
   styles: [
